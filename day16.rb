7512166237e1c9702d38bbdd69dec36d218505f1
Joao Amorim <i.psyduck@gmail.com>
2022-12-18 22:54:49 +0000

    dat 16 pt 1 twice as fast

--- day16.rb
+++ day16.rb
@@ -8,19 +8,73 @@ class Day16
       @valves[$1] = [$2.to_i, $3.split(", ")]
     end
     @map = {}
-    @valves.each do |k,v|
-      build_map(k) if v[0] > 0 || k == "AA"
+    build_map("AA")
+    good_valves.keys.each { |v| build_map(v) }
+  end
+
+  private def build_map(valve)
+    @map[valve] = {}
+    dist = { valve => 0 }
+    visited = { valve => true}
+    queue = [valve]
+
+    while queue.length > 0
+      v = queue.shift
+      @valves[v][1].each do |next_v|
+        next if visited[next_v]
+        dist[next_v] = dist[v] + 1
+        @map[valve][next_v] = dist[next_v] if @valves[next_v][0] > 0
+        visited[next_v] = true
+        queue << next_v
+      end
     end
   end
 
+  private def good_valves
+    @good_valves ||= @valves.select { |k, v| v[0] > 0 }
+  end
+
   def one
-    # flow, _ = best_move("AA", 30, good_valves.keys, {})
-    # flow
-    1
+    # Answer 1915
+    # Example 1651
+    solve_single("AA", 30, 0, {})
+  end
+
+  private def solve_single(valve, minutes_left, unopened, memo)
+    key = build_single_key(valve, minutes_left, unopened)
+    return memo[key] if memo[key]
+  
+    max_flow = 0
+    good_valves.keys.each_with_index do |v, i|
+      next if unopened & (2.pow(i)) > 0
+      next unless can_move?(valve, v, minutes_left)
+      dist = @map[valve][v]
+      flow = @valves[v][0] * (minutes_left - dist - 1)
+      total_flow = flow + solve_single(v, minutes_left - dist - 1, unopened | 2.pow(i), memo)
+      max_flow = [max_flow, total_flow].max
+    end
+
+    memo[key] = max_flow
+    max_flow
+  end
+
+  private def can_move?(valve, dest, minutes_left)
+    minutes_left - @map[valve][dest] - 1 >= 2
+  end
+
+  private def left_after_move(valve, dest, minutes_left)
+    minutes_left - @map[valve][dest] - 1
+  end
+
+  private def build_single_key(valve, minutes_left, unopened)
+    base = unopened*10000 + minutes_left*100
+    base + (valve == "AA" ? good_valves.keys.length : good_valves.keys.index(valve))
   end
 
   def two
-    2
+    # Answer 2772
+    # Example 1707
+   # solve_double("AA", 26, "AA", 26, 0, {}, true)
     #bound("AA", 30, good_valves.keys)
     # elephant = "AA"
     # me = "AA"
@@ -42,117 +96,188 @@ class Day16
     # total_flow
   end
 
-  private def solve
-    global_max = 0
-    candidates = [["AA", 30, good_valves.keys, 0]]
-    while candidates.length > 0
-      valve, minutes_left, unopened, curr_flow = candidates.shift
-      unopened.each do |v|
-        dist = @map[valve][v]
-        potential_max = bound(v, minutes_left - dist - 1, unopened - [v])
-        if curr_flow + potential_max > global_max
-          local_flow = @valves[v][0] * (minutes_left - dist - 1)
-          candidates << [v, minutes_left - dist - 1, unopened - [v], curr_flow + local_flow]
-        end
-      end
-    end
-    global_max
-  end
-
-  private def bound(location, minutes_left, unopened)
-    return 0 if minutes_left < 2
-    min_dist = unopened.map { |v| @map[v].map { |x| x[1] }.min }.min
-    max_flow = unopened.map { |v| @valves[v][0] }.max
-
-    flow = @valves[location][0] * (minutes_left - 1)
-    loop do
-      minutes_left -= min_dist + 1
-      break if minutes_left <= 0
-      flow += max_flow * minutes_left
-    end
-    flow
-  end
-
-  private def move(location, minutes_left, unopened)
-    _, path = best_move(location, minutes_left, unopened, {})
-    if path.length > 0
-      minutes_left = minutes_left - @map[location][path.first] - 1
-      location = path.first
-      flow = @valves[location][0] * minutes_left
-      unopened.delete(location)
-    else
-      minutes_left = 0
-      flow = 0
-    end
-    [location, minutes_left, flow]
-  end
-
-  private def build_map(valve)
-    @map[valve] = {}
-    dist = {}
-    visited = {}
-
-    queue = [valve]
-    dist[valve] = 0
-    visited[valve] = true
-
-    while queue.length > 0
-      v = queue.shift
-      @valves[v][1].each do |next_v|
-        next if visited[next_v]
-        dist[next_v] = dist[v] + 1
-        @map[valve][next_v] = dist[next_v] if @valves[next_v][0] > 0
-        visited[next_v] = true
-        queue << next_v
-      end
-    end
-  end
-
-  private def best_move(valve, minutes_left, unopened, memo)
-    key = "#{valve}-#{minutes_left}-#{unopened.sort.join(',')}"
-    return memo[key] if memo[key]
+  # private def solve_double(valve_me, minutes_left_me, valve_el, minutes_left_el, unopened, memo, log = false)
+  #   key = build_double_key(valve_me, minutes_left_me, valve_el, minutes_left_el, unopened)
+  #   return memo[key] if memo[key]
   
-    max_flow = 0
-    unopened.each do |v|
-      dist = @map[valve][v]
-      flow = @valves[v][0] * (minutes_left - dist - 1)
-      if flow > 0
-        f, p = best_move(v, minutes_left - dist - 1, unopened - [v], memo)
-        total_flow = flow + f
-        if total_flow > max_flow
-          max_flow = total_flow
-          path = [v] + p
-        end
-      end
-    end
+  #   max_flow = 0
+  #   good_valves.keys.each_with_index do |vi, i|
+  #     next if unopened & (2.pow(i)) > 0
+  #     next if minutes_left_el - @map[valve_el][vi] - 1 < 2 && minutes_left_me - @map[valve_me][vi] - 1 < 2
+  #     subpaths = 0
+  #     j = i + 1
+  #     while j < good_valves.keys.length
+  #       next if unopened & (2.pow(j)) > 0
+  #       vj = good_valves.keys[vj]
 
-    memo[key] = [max_flow, path]
-    [max_flow, path]
+
+  #       flow_i = @valves[vi][0] * (minutes_left_el - dist - 1)
+  #       if flow > 0
+  #         total_flow = flow + solve_double(valve_me, minutes_left_me, v, minutes_left_el - dist - 1, unopened | 2.pow(i), memo)
+  #         max_flow = [max_flow, total_flow].max
+  #       end
+
+  #   good_valves.keys.each_with_index do |v, i|
+  #     next if unopened & (2.pow(i)) > 0
+
+  #     puts "Solving #{v}" if log
+  #     dist = @map[valve_el][v]
+  #     flow = @valves[v][0] * (minutes_left_el - dist - 1)
+  #     if flow > 0
+  #       total_flow = flow + solve_double(valve_me, minutes_left_me, v, minutes_left_el - dist - 1, unopened | 2.pow(i), memo)
+  #       max_flow = [max_flow, total_flow].max
+  #     end
+
+  #     dist = @map[valve_me][v]
+  #     flow = @valves[v][0] * (minutes_left_me - dist - 1)
+  #     if flow > 0
+  #       total_flow = flow + solve_double(v, minutes_left_me - dist - 1, valve_el, minutes_left_el, unopened | 2.pow(i), memo)
+  #       max_flow = [max_flow, total_flow].max
+  #     end
+  #   end
+
+  #   memo[key] = max_flow
+  #   max_flow
+  # end
+
+  private def build_double_key(v_me, ml_me, v_el, ml_el, unopened)
+    key = unopened*100000000 + ml_me*1000000 + ml_el*10000
+    key += 100 * (v_me == "AA" ? good_valves.keys.length : good_valves.keys.index(v_me))
+    key += (v_el == "AA" ? good_valves.keys.length : good_valves.keys.index(v_el))
+    key
   end
 
-  private def best_move2(valve_e, minutes_left_e, valve_m, minutes_left_m, unopened, memo)
-    key = "#{valve_e}-#{minutes_left_e}-#{valve_m}-#{minutes_left_m}-#{unopened.sort.join(',')}"
-    return memo[key] if memo[key]
+  # private def solve
+  #   global_max = 0
+  #   candidates = [["AA", 30, good_valves.keys, 0]]
+  #   while candidates.length > 0
+  #     valve, minutes_left, unopened, curr_flow = candidates.shift
+  #     unopened.each do |v|
+  #       dist = @map[valve][v]
+  #       potential_max = bound(v, minutes_left - dist - 1, unopened - [v])
+  #       if curr_flow + potential_max > global_max
+  #         local_flow = @valves[v][0] * (minutes_left - dist - 1)
+  #         candidates << [v, minutes_left - dist - 1, unopened - [v], curr_flow + local_flow]
+  #       end
+  #     end
+  #   end
+  #   global_max
+  # end
+
+  # private def bound(location, minutes_left, unopened)
+  #   return 0 if minutes_left < 2
+  #   min_dist = unopened.map { |v| @map[v].map { |x| x[1] }.min }.min
+  #   max_flow = unopened.map { |v| @valves[v][0] }.max
+
+  #   flow = @valves[location][0] * (minutes_left - 1)
+  #   loop do
+  #     minutes_left -= min_dist + 1
+  #     break if minutes_left <= 0
+  #     flow += max_flow * minutes_left
+  #   end
+  #   flow
+  # end
+
+  # private def move(location, minutes_left, unopened)
+  #   _, path = best_move(location, minutes_left, unopened, {})
+  #   if path.length > 0
+  #     minutes_left = minutes_left - @map[location][path.first] - 1
+  #     location = path.first
+  #     flow = @valves[location][0] * minutes_left
+  #     unopened.delete(location)
+  #   else
+  #     minutes_left = 0
+  #     flow = 0
+  #   end
+  #   [location, minutes_left, flow]
+  # end
+
+
+  # private def best_move(valve, minutes_left, unopened, memo)
+  #   key = "#{valve}-#{minutes_left}-#{unopened.sort.join(',')}"
+  #   return memo[key] if memo[key]
   
-    max_flow = 0
-    unopened.each do |v|
-      dist = @map[valve_m][v]
-      flow = @valves[v][0] * (minutes_left_m - dist - 1)
-      move_me = flow > 0 ? flow + best_move2(valve_e, minutes_left_e, v, minutes_left_m - dist - 1, unopened - [v], memo) : 0
+  #   max_flow = 0
+  #   unopened.each do |v|
+  #     dist = @map[valve][v]
+  #     flow = @valves[v][0] * (minutes_left - dist - 1)
+  #     if flow > 0
+  #       total_flow = flow + best_move(v, minutes_left - dist - 1, unopened - [v], memo)
+  #       max_flow = [max_flow, total_flow].max
+  #     end
+  #   end
 
-      dist = @map[valve_e][v]
-      flow = @valves[v][0] * (minutes_left_e - dist - 1)
-      move_elephant = flow > 0 ? flow + best_move2(v, minutes_left_e - dist - 1, valve_m, minutes_left_m, unopened - [v], memo) : 0
+  #   memo[key] = max_flow
+  #   max_flow
+  # end
 
-      max_flow = [max_flow, move_me, move_elephant].max
-    end
+
+
+  # private def best_move4
+  #   queue = [[build_key("AA", 30, 0), 0]]
+  #   visited = {}
+  #   global_max = 0
+  #   while queue.length > 0
+  #     key, curr_flow = queue.shift
+  #     valve, minutes_left, unopened = read_key(key)
+  #     next if visited[key]
+  #     visited[key] = true
+  #     good_valves.keys.each_with_index do |v, i|
+  #       next if unopened & (2.pow(i)) > 0
+  #       #puts "#{valve} #{minutes_left} #{key}"
+  #       dist = @map[valve][v]
+  #       if minutes_left - dist - 1 < 2
+  #         global_max = [global_max, curr_flow].max
+  #       else
+  #         potential_max = bound(v, minutes_left - dist - 1, list_from_int(unopened | 2.pow(i)))
+  #         if curr_flow + potential_max > global_max
+  #           local_flow = @valves[v][0] * (minutes_left - dist - 1)
+  #           queue << [build_key(v, minutes_left - dist - 1, unopened | 2.pow(i)), curr_flow + local_flow]
+  #         end
+  #       end
+  #     end
+  #   end
     
-    memo[key] = max_flow
-    max_flow
-  end
+  #   global_max
+  # end
 
-  private def good_valves
-    @valves.select { |k, v| v[0] > 0 }
-  end
+  # private def list_from_int(unopened)
+  #   list = []
+  #   good_valves.keys.each_with_index do |v, i|
+  #     list << v unless unopened & (2.pow(i)) > 0
+  #   end
+  #   list 
+  # end
+
+  # private def read_key(key)
+  #   unopened = key / 10000
+  #   minutes_left = (key % 10000) / 100
+  #   idx = key % 100
+  #   valve = idx == good_valves.keys.length ? "AA" : good_valves.keys[idx]
+  #   [valve, minutes_left, unopened]
+  # end
+
+  # private def best_move2(valve_e, minutes_left_e, valve_m, minutes_left_m, unopened, memo)
+  #   key = "#{valve_e}-#{minutes_left_e}-#{valve_m}-#{minutes_left_m}-#{unopened.sort.join(',')}"
+  #   return memo[key] if memo[key]
+  
+  #   max_flow = 0
+  #   unopened.each do |v|
+  #     dist = @map[valve_m][v]
+  #     flow = @valves[v][0] * (minutes_left_m - dist - 1)
+  #     move_me = flow > 0 ? flow + best_move2(valve_e, minutes_left_e, v, minutes_left_m - dist - 1, unopened - [v], memo) : 0
+
+  #     dist = @map[valve_e][v]
+  #     flow = @valves[v][0] * (minutes_left_e - dist - 1)
+  #     move_elephant = flow > 0 ? flow + best_move2(v, minutes_left_e - dist - 1, valve_m, minutes_left_m, unopened - [v], memo) : 0
+
+  #     max_flow = [max_flow, move_me, move_elephant].max
+  #   end
+    
+  #   memo[key] = max_flow
+  #   max_flow
+  # end
+
+
 
 end
