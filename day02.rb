ee56dba99fcddb8f6f3d53b4264e5807851c4ee0
Joao Amorim <i.psyduck@gmail.com>
2022-12-21 14:59:09 +0000

    indent day 2

--- day02.rb
+++ day02.rb
@@ -8,9 +8,9 @@ class Day2
   def one
     points = 0
     @strategy.each do |s|
-    play = (s[1].ord - 23).chr
-    result = (play.ord - s[0].ord + 1) % 3
-    points += play.ord - 64 + result*3
+      play = (s[1].ord - 23).chr
+      result = (play.ord - s[0].ord + 1) % 3
+      points += play.ord - 64 + result*3
     end
     points
   end
@@ -18,17 +18,17 @@ class Day2
   def two
     points = 0
     @strategy.each do |s|
-    case s[1]
-    when "X"
-        play = (s[0].ord - 1 == 64 ? 67 : s[0].ord - 1).chr
-    when "Y"
-        play = s[0]
-        points += 3
-    when "Z"
-        play = (s[0].ord + 1 == 68 ? 65 : s[0].ord + 1).chr
-        points += 6
-    end
-    points += play.ord - 64
+      case s[1]
+      when "X"
+          play = (s[0].ord - 1 == 64 ? 67 : s[0].ord - 1).chr
+      when "Y"
+          play = s[0]
+          points += 3
+      when "Z"
+          play = (s[0].ord + 1 == 68 ? 65 : s[0].ord + 1).chr
+          points += 6
+      end
+      points += play.ord - 64
     end
     points
   end
