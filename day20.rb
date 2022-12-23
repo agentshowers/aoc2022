796252cd832240f23a3b2d352387279a56ac0cd7
Joao Amorim <i.psyduck@gmail.com>
2022-12-23 16:08:45 +0000

    better day 20

--- day20.rb
+++ day20.rb
@@ -3,8 +3,12 @@ class Day20
 
   def initialize
     @zero = nil
-    @code = File.read(INPUT).split("\n").map do |x|
-      node = Node.new(x.to_i)
+    lines = File.readlines(INPUT, chomp: true)
+    @bucket_list = BucketList.new(lines.length)
+    @code = lines.each_with_index.map do |x, i|
+      b = i / BucketList::SIZE
+      node = Node.new(x.to_i, b)
+      @bucket_list.buckets[b] << node
       @zero = node if x.to_i == 0
       node
     end
@@ -23,23 +27,98 @@ class Day20
     list = @code.dup
     (1..n).each do
       @code.each do |node|
-        idx = list.index(node)
+        idx = @bucket_list.index(node)
         move_count = (node.x * key) % (size - 1)
         new_idx = (idx + move_count) % (size - 1)
-        list.delete_at(idx)
-        list.insert(new_idx, node)
+        @bucket_list.shift(node, idx, new_idx)
       end
     end
-    zero_idx = list.index(@zero)
-    sum = list[(zero_idx + 1000) % size].x + list[(zero_idx + 2000) % size].x + list[(zero_idx + 3000) % size].x
-    sum * key
+    zero_idx = @bucket_list.index(@zero)
+    [1000, 2000, 3000].map do |i|
+      @bucket_list.get((zero_idx + i) % size).x
+    end.sum * key
+  end
+end
+
+class BucketList
+  SIZE = 250
+
+  attr_accessor :buckets
+
+  def initialize(n)
+    n_buckets = (1.0 * n / SIZE).ceil
+    @buckets = Array.new(n_buckets) { [] }
+  end
+
+  def index(node)
+    node.bucket * SIZE + buckets[node.bucket].index(node)
+  end
+
+  def get(index)
+    bucket = index / SIZE
+    @buckets[bucket][index % SIZE]
+  end
+
+  def shift(node, from, to)
+    if to > from
+      dir, grab, place = [1, 0, SIZE-1]
+    else
+      dir, grab, place = [-1, SIZE-1, 0]
+    end
+
+    orig = from / SIZE
+    dest = to / SIZE
+    @buckets[orig].delete_at(from % SIZE)
+    while orig != dest
+      shifted_node = @buckets[orig + dir][grab]
+      shifted_node.bucket = orig
+      @buckets[orig + dir].delete_at(grab)
+      @buckets[orig].insert(place, shifted_node)
+      orig += dir
+    end
+    @buckets[orig].insert(to % SIZE, node)
+    node.bucket = orig
+  end
+
+  private def shift_right(node, from, to)
+    orig = from / SIZE
+    dest = to / SIZE
+    @buckets[orig].delete_at(from % SIZE)
+    while orig < dest
+      shifted_node = @buckets[orig + 1][0]
+      shifted_node.bucket = orig
+      @buckets[orig + 1].delete_at(0)
+      @buckets[orig] << shifted_node
+      
+      orig += 1
+    end
+    @buckets[orig].insert(to % SIZE, node)
+    node.bucket = orig
+  end
+
+  private def shift_left(node, from, to)
+    orig = from / SIZE
+    dest = to / SIZE
+    @buckets[orig].delete_at(from % SIZE)
+    while orig > dest
+      shifted_node = @buckets[orig - 1][SIZE-1]
+      shifted_node.bucket = orig
+      @buckets[orig - 1].delete_at(SIZE-1)
+      @buckets[orig].insert(0, shifted_node)
+      
+      orig -= 1
+    end
+    @buckets[orig].insert(to % SIZE, node)
+    node.bucket = orig
   end
 end
 
 class Node
   attr_reader :x
+  attr_accessor :bucket
 
-  def initialize(x)
+  def initialize(x, bucket)
     @x = x
+    @bucket = bucket
   end
 end
