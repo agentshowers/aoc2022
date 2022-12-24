81f30370aa958b63b2522b3e653c3550d72b1e49
Joao Amorim <i.psyduck@gmail.com>
2022-12-24 09:49:52 +0000

    Slow day 24 (works for input, but not example ?)

---
+++ day24.rb
@@ -0,0 +1,82 @@
+class Day24
+  INPUT = "day24.input"
+  DIRECTIONS = [[1, 0], [0, 1], [-1, 0], [0, -1], [0, 0]]
+
+  def initialize
+    lines = File.readlines(INPUT, chomp: true)
+    @rows = lines.length - 2
+    @columns = lines[0].length - 2
+    @row_patterns = Array.new(@rows) { [] }
+    @column_patterns = Array.new(@columns) { [] }
+    @lcm = @rows.lcm(@columns)
+
+    lines.each_with_index do |line, j|
+      next if j == 0 || j > @rows
+      line.chars.each_with_index do |c, i|
+        next if i == 0 || i > @columns
+        @row_patterns[j-1] << [i-1, 1] if c == ">"
+        @row_patterns[j-1] << [i-1, -1] if c == "<"
+        @column_patterns[i-1] << [j-1, 1] if c == "v"
+        @column_patterns[i-1] << [j-1, -1] if c == "^"
+      end
+    end
+  end
+
+  def one
+    @first_trip = bfs(0, 0, @columns - 1, @rows - 1, 0)
+    @first_trip
+  end
+
+  def two
+    second_trip = bfs(@columns - 1, @rows - 1, 0, 0, @first_trip)
+    bfs(0, 0, @columns - 1, @rows - 1, second_trip)
+  end
+
+  private def bfs(start_x, start_y, end_x, end_y, start)
+    loop do
+      start += 1
+      break if free(start_x, start_y, start)
+    end
+    init_key = encode(start_x, start_y, start)
+    queue = [init_key]
+    visited = {}
+    dist = { init_key => start }
+    states = 0
+    while queue.length > 0
+      key = queue.shift
+      
+      x, y, minutes = parse(key)
+      DIRECTIONS.each do |dx, dy|
+        nx, ny, nmins = [x+dx, y+dy, minutes+1]
+        nkey = encode(nx, ny, nmins)
+        if !visited[nkey] && in_bounds?(nx, ny) && free(nx, ny, nmins)
+          visited[nkey] = true
+          return dist[key] + 2 if nx == end_x && ny == end_y
+          queue << nkey
+          dist[nkey] = dist[key] + 1
+        end
+      end
+    end
+  end
+
+  private def in_bounds?(x, y)
+    x >= 0 && x < @columns && y >= 0 && y < @rows
+  end
+
+  private def free(x, y, minutes)
+    vert_free = @column_patterns[x].none? { |j, d| (j + minutes*d) % @rows == y }
+    hor_free = @row_patterns[y].none? { |i, d| (i + minutes*d) % @columns == x }
+    vert_free && hor_free
+  end
+
+  private def encode(x, y, minutes)
+    x + y*1000 + (minutes % @lcm)*1000000
+  end
+
+  private def parse(key)
+    x = key % 1000
+    y = (key % 1000000) / 1000
+    minutes = key / 1000000
+    [x, y, minutes]
+  end
+end
--- main.rb
+++ main.rb
@@ -23,13 +23,14 @@ SOLUTIONS = [
   [8028, 8798438007673],
   [256997859093114, 3952288690726],
   [122082, 134076],
-  [4091, 1036]
+  [4091, 1036],
+  [253, 794]
 ]
-DAYS = 23
+DAYS = 24
 
 total_time = 0
 
-(1..DAYS).each do |n|
+(24..DAYS).each do |n|
   require_relative "day#{n.to_s.rjust(2, '0')}.rb"
 
   day_class = Kernel.const_get("Day#{n}")
