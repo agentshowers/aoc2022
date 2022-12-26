c75a0c1397aa79862e7692d87b08d2dbd8833498
Joao Amorim <i.psyduck@gmail.com>
2022-12-26 23:11:33 +0000

    Day 19 improvements + dfs wip

--- day19.rb
+++ day19.rb
@@ -46,7 +46,8 @@ class Day19
       geode = acc[key]
       minutes, ore, clay, obs, ore_r, clay_r, obs_r, geode_r = read_key(key)
       if ceiling(geode, geode_r, minutes) >= @global_floor
-        candidates(ore, clay, obs, geode, ore_r, clay_r, obs_r, geode_r, blueprint, minutes).each do |o,c,ob,g,o_r,c_r,ob_r,g_r,mins|
+        candidates(ore, clay, obs, ore_r, clay_r, obs_r, geode_r, blueprint, minutes).each do |o,c,ob,g,o_r,c_r,ob_r,g_r,mins|
+          g += geode
           if mins > @max_minutes
             max_g = [g, max_g].max
           else
@@ -65,39 +66,81 @@ class Day19
     max_g
   end
 
-  private def candidates(ore, clay, obs, geode, ore_r, clay_r, obs_r, geode_r, blueprint, minutes)
-    resources = [ore, clay, obs, geode]
+  private def solve_dfs(blueprint)
+    @global_floor = 0
+    init_key = build_key(1,0,0,0,1,0,0,0)
+    dfs(blueprint, init_key, 0, {})
+  end
+
+  private def dfs(blueprint, key, geodes, memo)
+    return memo[key] if memo[key]
+
+    #puts read_key(key).to_s
+    minutes, ore, clay, obs, ore_r, clay_r, obs_r, geode_r = read_key(key)
+    max_g = candidates(ore, clay, obs, ore_r, clay_r, obs_r, geode_r, blueprint, minutes).map do |o,c,ob,g,o_r,c_r,ob_r,g_r,mins|
+      if mins > @max_minutes
+        g
+      else
+        key = build_key(mins,o,c,ob,o_r,c_r,ob_r,g_r)
+        #@global_floor = [@global_floor, floor(g, g_r, mins)].max
+        g + dfs(blueprint, key, g + geodes, memo)
+      end
+    end.max
+
+    memo[key] = max_g
+    max_g
+  end
+
+  private def candidates(ore, clay, obs, ore_r, clay_r, obs_r, geode_r, blueprint, minutes)
+    resources = [ore, clay, obs, 0]
     robots = [ore_r, clay_r, obs_r, geode_r]
     candidates = []
     if minutes == @max_minutes
-      candidates << [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      candidates << [[0, 0, 0, 0], [0, 0, 0, 0], 0]
     else
       time_left = @max_minutes - minutes
 
-      costs = blueprint[3]
       has_time, build_time = can_build_robot?(blueprint, robots, resources, 3, time_left)
-      candidates << build_robot(costs, robots, resources, build_time) + [0, 0, 0, 1, build_time] if has_time
+      candidates << [blueprint[3], [0, 0, 0, 1], build_time] if has_time
 
       if build_time.abs > 0
         obs_needed = robot_needed?(blueprint, robots, resources, 2, time_left)
         has_time, build_time = can_build_robot?(blueprint, robots, resources, 2, time_left)
-        candidates << build_robot(blueprint[2], robots, resources, build_time) + [0, 0, 1, 0, build_time] if obs_needed && has_time
+        candidates << [blueprint[2], [0, 0, 1, 0], build_time] if obs_needed && has_time
 
         clay_needed = obs_needed && robot_needed?(blueprint, robots, resources, 1, time_left)
         has_time, build_time = can_build_robot?(blueprint, robots, resources, 1, time_left)
-        candidates << build_robot(blueprint[1], robots, resources, build_time) + [0, 1, 0, 0, build_time] if clay_needed && has_time
+        candidates << [blueprint[1], [0, 1, 0, 0], build_time] if clay_needed && has_time
 
         if time_left >= blueprint[0][0] + 2
           ore_needed = robot_needed?(blueprint, robots, resources, 0, time_left)
           has_time, build_time = can_build_robot?(blueprint, robots, resources, 0, time_left)
-          candidates << build_robot(blueprint[0], robots, resources, build_time) + [1, 0, 0, 0, build_time] if ore_needed && has_time
+          candidates << [blueprint[0], [1, 0, 0, 0], build_time] if ore_needed && has_time
         end
 
-        candidates << [0, 0, 0, 0, 0, 0, 0, 0, 0] if candidates.length == 0
+        candidates << [[0, 0, 0, 0], [0, 0, 0, 0], 0] if candidates.length == 0
       end
     end
-    candidates.map do |a,b,c,d,e,f,g,h,mins|
-      [ore + ore_r + a, clay + clay_r + b, obs + obs_r + c, geode + geode_r + d, ore_r + e, clay_r + f, obs_r + g, geode_r + h, minutes + mins + 1]
+
+    ore_needs = blueprint.map { |b| b[0] }.max
+    clay_needs = blueprint[2][1]
+    obs_needs = blueprint[3][2]
+
+    candidates.map do |costs, robots, mins|
+      new_ore = ore + ore_r*(mins+1) - costs[0]
+      new_clay = clay + clay_r*(mins+1) - costs[1]
+      new_obs = obs + obs_r*(mins+1) - costs[2]
+      [
+        (ore_r + robots[0] == ore_needs ? [ore_needs, new_ore].min : new_ore),
+        (clay_r + robots[1] == clay_needs ? [clay_needs, new_clay].min : new_clay),
+        (obs_r + robots[2] == obs_needs ? [obs_needs, new_obs].min : new_obs),
+        geode_r*(mins+1),
+        ore_r + robots[0],
+        clay_r + robots[1],
+        obs_r + robots[2],
+        geode_r + robots[3],
+        minutes + mins + 1
+      ]
     end
   end
 
