7e250930fee7a63fc2025f972b09bd634c3e5839
Joao Amorim <i.psyduck@gmail.com>
2022-12-28 19:16:20 +0000

    rejig blueprints

--- day19.rb
+++ day19.rb
@@ -4,10 +4,7 @@ class Day19
   def initialize
     @blueprints = File.readlines(INPUT, chomp: true).map do |line|
       line =~ /Blueprint \d+\: Each ore robot costs (\d+) ore\. Each clay robot costs (\d+) ore\. Each obsidian robot costs (\d+) ore and (\d+) clay\. Each geode robot costs (\d+) ore and (\d+) obsidian\./
-      [[$1.to_i, 0, 0, 0],
-      [$2.to_i, 0, 0, 0],
-      [$3.to_i, $4.to_i, 0, 0],
-      [$5.to_i, 0, $6.to_i, 0]]
+      [$1.to_i, $2.to_i, $3.to_i, $4.to_i, $5.to_i, $6.to_i]
     end
   end
 
@@ -59,38 +56,38 @@ class Day19
     resources = [ore, clay, obs, 0]
     robots = [ore_r, clay_r, obs_r, geode_r]
     candidates = []
-    ore_needs = [blueprint[3][0], blueprint[2][0], blueprint[1][0]].max
-    clay_needs = blueprint[2][1]
-    obs_needs = blueprint[3][2]
+    ore_needs = [blueprint[4], blueprint[2], blueprint[1]].max
+    clay_needs = blueprint[3]
+    obs_needs = blueprint[5]
 
     if minutes == @max_minutes
-      candidates << build_robot(minutes, ore, clay, obs, ore_r, clay_r, obs_r, geode_r, ore_needs, clay_needs, obs_needs, [0, 0, 0, 0], [0, 0, 0, 0], 0)
+      candidates << build_robot(minutes, ore, clay, obs, ore_r, clay_r, obs_r, geode_r, ore_needs, clay_needs, obs_needs, [0, 0, 0], [0, 0, 0, 0], 0)
     else
       time_left = @max_minutes - minutes
       
-      build_time = time_for_geode(blueprint[3], robots, resources)
+      build_time = time_for_geode(blueprint[4], blueprint[5], robots, resources)
       has_time = can_build_robot?(build_time, time_left)
-      candidates << build_robot(minutes, ore, clay, obs, ore_r, clay_r, obs_r, geode_r, ore_needs, clay_needs, obs_needs, blueprint[3], [0, 0, 0, 1], build_time) if has_time
+      candidates << build_robot(minutes, ore, clay, obs, ore_r, clay_r, obs_r, geode_r, ore_needs, clay_needs, obs_needs, [blueprint[4], 0, blueprint[5]], [0, 0, 0, 1], build_time) if has_time
 
       if build_time.abs > 0
         obs_needed = robot_needed?(obs_needs, obs_r, obs, time_left, 3)
-        build_time = time_for_obs(blueprint[2], robots, resources)
+        build_time = time_for_obs(blueprint[2], blueprint[3], robots, resources)
         has_time = can_build_robot?(build_time, time_left)
-        candidates << build_robot(minutes, ore, clay, obs, ore_r, clay_r, obs_r, geode_r, ore_needs, clay_needs, obs_needs, blueprint[2], [0, 0, 1, 0], build_time) if obs_needed & has_time
+        candidates << build_robot(minutes, ore, clay, obs, ore_r, clay_r, obs_r, geode_r, ore_needs, clay_needs, obs_needs, [blueprint[2], blueprint[3], 0], [0, 0, 1, 0], build_time) if obs_needed & has_time
 
         clay_needed = obs_needed && robot_needed?(clay_needs, clay_r, clay, time_left, 5)
         build_time = time_for_clay(blueprint[1], robots, resources)
         has_time = can_build_robot?(build_time, time_left)
-        candidates << build_robot(minutes, ore, clay, obs, ore_r, clay_r, obs_r, geode_r, ore_needs, clay_needs, obs_needs, blueprint[1], [0, 1, 0, 0], build_time) if clay_needed & has_time
+        candidates << build_robot(minutes, ore, clay, obs, ore_r, clay_r, obs_r, geode_r, ore_needs, clay_needs, obs_needs, [blueprint[1], 0, 0], [0, 1, 0, 0], build_time) if clay_needed & has_time
 
-        if time_left >= blueprint[0][0] + 2
+        if time_left >= blueprint[0] + 2
           ore_needed = robot_needed?(ore_needs, ore_r, ore, time_left, 3)
           build_time = time_for_ore(blueprint[0], robots, resources)
           has_time = can_build_robot?(build_time, time_left)
-          candidates << build_robot(minutes,ore, clay, obs, ore_r, clay_r, obs_r, geode_r, ore_needs, clay_needs, obs_needs, blueprint[0], [1, 0, 0, 0], build_time) if ore_needed & has_time
+          candidates << build_robot(minutes,ore, clay, obs, ore_r, clay_r, obs_r, geode_r, ore_needs, clay_needs, obs_needs, [blueprint[0], 0, 0], [1, 0, 0, 0], build_time) if ore_needed & has_time
         end
 
-        candidates << build_robot(minutes, ore, clay, obs, ore_r, clay_r, obs_r, geode_r, ore_needs, clay_needs, obs_needs, [0, 0, 0, 0], [0, 0, 0, 0], 0) if candidates.length == 0
+        candidates << build_robot(minutes, ore, clay, obs, ore_r, clay_r, obs_r, geode_r, ore_needs, clay_needs, obs_needs, [0, 0, 0], [0, 0, 0, 0], 0) if candidates.length == 0
       end
     end
 
@@ -131,30 +128,30 @@ class Day19
     (1.0 * (cost - resource) / robot).ceil
   end
 
-  private def time_for_geode(costs, robots, resources)
+  private def time_for_geode(ore_cost, obs_cost, robots, resources)
     return -1 if robots[0] == 0 || robots[2] == 0
 
-    ore_time = time_for_resource(costs[0], robots[0], resources[0])
-    obs_time = time_for_resource(costs[2], robots[2], resources[2])
+    ore_time = time_for_resource(ore_cost, robots[0], resources[0])
+    obs_time = time_for_resource(obs_cost, robots[2], resources[2])
     [ore_time, obs_time].max
   end
 
-  private def time_for_obs(costs, robots, resources)
+  private def time_for_obs(ore_cost, clay_cost, robots, resources)
     return -1 if robots[0] == 0 || robots[1] == 0
 
-    ore_time = time_for_resource(costs[0], robots[0], resources[0])
-    clay_time = time_for_resource(costs[1], robots[1], resources[1])
+    ore_time = time_for_resource(ore_cost, robots[0], resources[0])
+    clay_time = time_for_resource(clay_cost, robots[1], resources[1])
     [ore_time, clay_time].max
   end
 
-  private def time_for_clay(costs, robots, resources)
+  private def time_for_clay(ore_cost, robots, resources)
     return -1 if robots[0] == 0
 
-    time_for_resource(costs[0], robots[0], resources[0])
+    time_for_resource(ore_cost, robots[0], resources[0])
   end
 
-  private def time_for_ore(costs, robots, resources)
-    time_for_resource(costs[0], robots[0], resources[0])
+  private def time_for_ore(ore_cost, robots, resources)
+    time_for_resource(ore_cost, robots[0], resources[0])
   end
 
   private def floor(geode, geode_r, minutes)
@@ -163,15 +160,15 @@ class Day19
   end
   
   private def ceiling(ore, obs, geode, ore_r, obs_r, geode_r, minutes, blueprint)
-    if obs >= blueprint[3][2]
+    if obs >= blueprint[5]
       time_for_obs = 0
     else
-      time_for_obs = ((0.5 - obs_r) + Math.sqrt((0.5 - obs_r)**2 + 2*(blueprint[3][2] - obs))).ceil
+      time_for_obs = ((0.5 - obs_r) + Math.sqrt((0.5 - obs_r)**2 + 2*(blueprint[5] - obs))).ceil
     end
-    if ore >= blueprint[3][0]
+    if ore >= blueprint[4]
       time_for_ore = 0
     else
-      time_for_ore = ((0.5 - ore_r) + Math.sqrt((0.5 - ore_r)**2 + 2*(blueprint[3][0] - ore))).ceil
+      time_for_ore = ((0.5 - ore_r) + Math.sqrt((0.5 - ore_r)**2 + 2*(blueprint[4] - ore))).ceil
     end
     minutes_left = @max_minutes - minutes + 1
     current_production = geode + geode_r*minutes_left
