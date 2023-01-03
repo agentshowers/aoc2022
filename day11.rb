34925fe703780ae2aebdcf57c80e35862ae04226
Joao Amorim <i.psyduck@gmail.com>
2023-01-03 17:06:12 +0000

    Gain 40 ms with random bullshit

--- day11.rb
+++ day11.rb
@@ -3,10 +3,11 @@ class Day11
 
   def initialize
     @monkeys = []
+    @initial_items = []
 
     File.readlines(INPUT, chomp: true).each_slice(7) do |slice|
+      @initial_items <<  slice[1].split(": ")[1].split(", ").map(&:to_i)
       monkey = []
-      monkey <<  slice[1].split(": ")[1].split(", ").map(&:to_i)
       slice[2] =~ /Operation: new = old (\+|\*) (old|\d+)/
       monkey << $1
       monkey << ($2 == "old" ? 0 : $2.to_i)
@@ -21,29 +22,16 @@ class Day11
   end
 
   def one
-    run(20) do |item|
-      item / 3
-    end
-  end
-  
-  def two
-    div = @monkeys.map { |m| m[3] }.inject(:*)
-    run(10000) do |item|
-      item % div
-    end
-  end
-
-  private def run(rounds)
     activity = Array.new(@monkeys.length, 0)
-    items = @monkeys.map{ |m| m[0].dup }
+    items = @initial_items.map{ _1.dup }
   
-    (1..rounds).each do
-      @monkeys.each_with_index do |(_, op, op_r, tst, t, f), i|
+    (1..20).each do
+      @monkeys.each_with_index do |(op, op_r, tst, t, f), i|
         activity[i] += items[i].length
         items[i].each do |item|
-          right = op_r + (1 - (op_r <=> 0)) * item
-          item = item.send(op, right)
-          item = yield(item)
+          right = op_r == 0 ? item : op_r
+          item = op == "*" ? item * right : item + right
+          item = item / 3
           if item % tst == 0
             items[t] << item
           else
@@ -56,5 +44,32 @@ class Day11
 
     activity.sort.reverse[..1].inject(:*)
   end
+  
+  def two
+    div = @monkeys.map { |m| m[2] }.inject(:*)
+
+    activity = Array.new(@monkeys.length, 0)
+    items = @initial_items.map{ _1.dup }
+  
+    (1..10000).each do
+      @monkeys.each_with_index do |(op, op_r, tst, t, f), i|
+        activity[i] += items[i].length
+        items[i].each do |item|
+          right = op_r == 0 ? item : op_r
+          item = op == "*" ? item * right : item + right
+          item = item % div
+          if item % tst == 0
+            items[t] << item
+          else
+            items[f] << item
+          end
+        end
+        items[i] = []
+      end
+    end
+
+    activity.sort.reverse[..1].inject(:*)
+  end
+
 end
 
